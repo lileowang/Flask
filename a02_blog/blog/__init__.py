0822db025087ecd22f39bea425a8babd3bb96965
lileowang <wangli20@hotmail.com>
2020-08-21 22:41:28 -0700

    use render templates.

--- a02_blog/blog/__init__.py
+++ a02_blog/blog/__init__.py
@@ -1,15 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
+from datetime import datetime
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
 
     @app.route('/')
     def index():
-        return 'index page'
+        heading = 'index page'
+        now = datetime.now()
+        return render_template('index.html', heading=heading, now=now)
 
     @app.route('/<string:name>')
     def test(name):
         name = name.capitalize()
-        return f'hello, {name}'
+        days = ['mon', 'tue', 'wed']
+        return render_template('test.html', name=name, days=days)
 
     return app
